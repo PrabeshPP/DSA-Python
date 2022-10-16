f9603920929e8a87205b3fc85ca51afe75aef93c
Prabesh <prabesh.bista369@gmail.com>
2022-10-16 19:42:59 -0400

    [adde]d

---
+++ LinkedList/LinkedList.py
@@ -0,0 +1,10 @@
+class LinkedList:
+    def __init__(self) -> None:
+        self.head=None
+        
+    def printList(self):
+        temp=self.head
+        while(temp!=None):
+            print(temp.data)
+            temp=temp.next
+            
---
+++ LinkedList/main.py
@@ -0,0 +1,20 @@
+from IntNode import Node
+from LinkedList import LinkedList
+
+
+
+
+if __name__=="__main__":
+    llist=LinkedList()
+    llist.head=Node(3)
+    secondNode=Node(10)
+    thirdNode=Node(20)
+    
+    # Linking two nodes
+    llist.head.next=secondNode
+    secondNode.next=thirdNode
+    
+    
+    llist.printList()
+    
+    

36b6e24bf60739607bdb993d498973a33daedc74
PrabeshPP <prabesh.bista369@gmail.com>
2022-10-18 19:36:04 -0400

    [added] optimised solution for twosums

---
+++ LeetCode/Easy/Twosum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dict={}
+        for key,value in enumerate(nums):
+            difference=target-value
+            if difference in dict:
+                return [dict[difference],key]
+            difference[value]=key

0fb2e51ef965abd678be6198966f61e88534740a
PrabeshPP <prabesh.bista369@gmail.com>
2022-10-23 20:20:49 -0400

    [implemented] doublyLinkedList using python

--- Doubly-LinkedList/IntNode.py
+++ Doubly-LinkedList/IntNode.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 
 
 class Node:
-    def __init__(self,data,prev,next) -> None:
+    def __init__(self,data,prev=None,next=None) -> None:
         self.data=data
         self.prev=None
         self.next=None
